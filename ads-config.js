38ab51fa288a814b47d9bd844836ca8c3f287a1e
flybyraceapp <flybyraceapp@gmail.com>
2026-07-26 12:55:59 +0900

    add ads

--- ads-config.js
+++ ads-config.js
@@ -1,5 +1,5 @@
 window.FBR_ADS_CONFIG = Object.freeze({
-  // Public AdSense values. Fill these after flybyrace.com is approved in AdSense.
-  publisherId: "",
+  // Public AdSense values. Add the banner slot after creating the manual ad unit.
+  publisherId: "ca-pub-4970628766217975",
   bannerSlot: ""
 });
